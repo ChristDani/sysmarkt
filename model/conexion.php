b71f1e21de70cc56a2fda11fad6d15c8eff91d43
Christian Daniel Campaña Díaz <christiand0393@gmail.com>
2023-02-17 12:25:28 -0500

    yap

--- model/conexion.php
+++ model/conexion.php
@@ -8,25 +8,6 @@
 			try 
 			{
 				$servidor = "localhost";
-				$usuario = "root";
-				$clave = "";
-				$base = "argosal"; 
-
-				$con = mysqli_connect("$servidor", "$usuario", "$clave", "$base");
-	
-				if ($con) 
-				{
-					return $con;
-				}
-			} 
-			catch (Exception $e) 
-			{
-				// echo 'Excepción capturada: ',  $e->getMessage(), "\n";
-			}
-
-			try 
-			{
-				$servidor = "localhost";
 				$usuario = "u292896214_root";
 				$clave = "3yDd#R4ccZA/";
 				$base = "u292896214_argosal";
@@ -41,7 +22,26 @@
 			catch (Exception $e) 
 			{
 				// echo 'Excepción capturada: ',  $e->getMessage(), "\n";
-			}		
+			}	
+			
+			try 
+			{
+				$servidor = "localhost";
+				$usuario = "root";
+				$clave = "";
+				$base = "argosal"; 
+
+				$con = mysqli_connect("$servidor", "$usuario", "$clave", "$base");
+	
+				if ($con) 
+				{
+					return $con;
+				}
+			} 
+			catch (Exception $e) 
+			{
+				// echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+			}	
 		}
 
 		public function desconectar($con)
